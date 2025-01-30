dc10be7ede596206bbd62518e32894444f7ab982
unique-jakub <jakub@unique.ch>
2025-01-30 17:12:36 +0100

    enable public network access for redis

--- 10_azure/modules/workloads/redis.tf
+++ 10_azure/modules/workloads/redis.tf
@@ -3,7 +3,7 @@ module "redis" {
   name                          = var.redis_name
   resource_group_name           = data.azurerm_resource_group.sensitive.name
   location                      = data.azurerm_resource_group.sensitive.location
-  public_network_access_enabled = false
+  public_network_access_enabled = true
   tags                          = var.tags
   key_vault_id                  = var.sensitive_kv_id
 }
