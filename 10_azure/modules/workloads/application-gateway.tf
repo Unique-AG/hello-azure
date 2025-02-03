aa517d202ee2aa25358e57968f37ab5086eca643
Sandro Camastral <124629881+unique-sandro@users.noreply.github.com>
2025-02-03 17:44:44 +0100

    chore(infra): set WAF to detection mode (#19)

--- 10_azure/modules/workloads/application-gateway.tf
+++ 10_azure/modules/workloads/application-gateway.tf
@@ -5,6 +5,7 @@ module "application_gateway" {
   resource_group_name        = data.azurerm_resource_group.core.name
   resource_group_location    = data.azurerm_resource_group.core.location
   subnet_appgw               = var.subnet_agw_id
+  gateway_mode               = "Detection"
   gateway_sku                = "WAF_v2"
   gateway_tier               = "WAF_v2"
   private_ip                 = cidrhost(var.subnet_agw_cidr, 6)
