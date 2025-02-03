d0652164f6f4acfd31c09fbc9ab37497ba59dac7
unique-jakub <jakub@unique.ch>
2025-02-03 12:36:36 +0100

    add azapi provider definition

--- 10_azure/providers.tf
+++ 10_azure/providers.tf
@@ -12,3 +12,10 @@ provider "azuread" {
   use_oidc  = var.use_oidc
   client_id = var.client_id
 }
+
+provider "azapi" {
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  use_oidc        = var.use_oidc
+  client_id       = var.client_id
+}
