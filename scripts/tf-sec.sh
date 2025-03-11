2738021af8defa40249651f19cb0581813cb4fd6
Michael Dreher <michael@unique.ch>
2025-03-11 17:03:51 +0100

    chore: always upload sarif results

--- scripts/tf-sec.sh
+++ scripts/tf-sec.sh
@@ -5,17 +5,14 @@
 # SPDX-SnippetEnd
 echo "Running tf-sec"
 
-# Create the output file with write permissions before mounting, so the docker run command can write to it
-if [ "${GITHUB_ACTIONS}" = "true" ]; then
-  touch tfsec-results.sarif
-  chmod 666 tfsec-results.sarif
-fi
-
 # Base command
 CMD="docker run --rm -v "$(pwd):/workdir" aquasec/tfsec /workdir --config-file /workdir/.github/configs/tfsec.yaml"
 
 # Add SARIF output options only in GitHub Actions
 if [ "${GITHUB_ACTIONS}" = "true" ]; then
+  # Create the output file with write permissions before mounting, so the docker run command can write to it
+  touch tfsec-results.sarif
+  chmod 666 tfsec-results.sarif
   CMD="$CMD --format sarif --out /workdir/tfsec-results.sarif"
 fi
 
