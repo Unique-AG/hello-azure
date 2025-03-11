96f978f7a498cfed5b8c848ad3a2823de7e64c66
Michael Dreher <michael@unique.ch>
2025-03-11 16:35:32 +0100

    chore: add tfsec results to GH security overview

--- scripts/tf-sec.sh
+++ scripts/tf-sec.sh
@@ -4,4 +4,7 @@
 # SPDX-SnippetCopyrightText: 2024 © Unique AG
 # SPDX-SnippetEnd
 echo "Running tf-sec"
-docker run --rm -v "$(pwd):/workdir" aquasec/tfsec /workdir --config-file /workdir/.github/configs/tfsec.yaml
+docker run --rm -v "$(pwd):/workdir" aquasec/tfsec /workdir \
+  --config-file /workdir/.github/configs/tfsec.yaml \
+  --format sarif \
+  --out /workdir/tfsec-results.sarif
