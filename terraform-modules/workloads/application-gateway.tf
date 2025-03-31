856276553f555c0058681c11689a1b070709cfdc
unique-jakub <jakub@unique.ch>
2025-03-31 11:14:54 +0100

    test disabling public ip on the application gateway

--- terraform-modules/workloads/application-gateway.tf
+++ terraform-modules/workloads/application-gateway.tf
@@ -11,4 +11,5 @@ module "application_gateway" {
   private_ip                 = cidrhost(var.subnet_agw_cidr, 6)
   log_analytics_workspace_id = var.log_analytics_workspace_id
   ip_name                    = var.ip_name
+  public_ip_enabled          = false # FIXME: revert to using public IP once confirmed that using private IP alone is enough
 }
