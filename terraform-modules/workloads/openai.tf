5d3d7b1055c40a5879539c0d6af3f56a6a7516f1
unique-jakub <jakub@unique.ch>
2025-06-03 11:45:18 +0200

    fix: allow public access to mdi (#95)

--- terraform-modules/workloads/openai.tf
+++ terraform-modules/workloads/openai.tf
@@ -45,7 +45,10 @@ module "document_intelligence" {
     "swedencentral-form-recognizer" = {
       location = "swedencentral"
       custom_subdomain_name = var.document_intelligence_custom_subdomain_name
+      public_network_access_enabled = true # FIXME: use private endpoints'
+      local_auth_enabled = true # https://github.com/Unique-AG/terraform-modules/issues/79
     }
   }
-  key_vault_id               = var.main_kv_id
+  #key_vault_id               = var.main_kv_id
 }
+
