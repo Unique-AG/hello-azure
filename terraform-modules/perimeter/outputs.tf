f75951d9cf6d2a4439402b4243aebcfd68d1bead
unique-jakub <jakub@unique.ch>
2025-06-27 13:54:44 +0200

    fix rebase oopsie 🌭

--- terraform-modules/perimeter/outputs.tf
+++ terraform-modules/perimeter/outputs.tf
@@ -45,10 +45,6 @@ output "storage_private_dns_zone_id" {
   description = "ID of the storage private DNS zone"
   value       = azurerm_private_dns_zone.private_dns_zones["storage"].id
 }
-output "storage_private_dns_zone_id" {
-  description = "ID of the storage private DNS zone"
-  value       = azurerm_private_dns_zone.private_dns_zones["storage"].id
-}
 
 output "dns_zone_name_servers" {
   description = "The Name Servers for the DNS zone"
@@ -97,44 +93,4 @@ output "private_dns_zone_redis_id" {
 output "private_dns_zone_psql_id" {
   description = "ID of the psql private DNS zone"
   value       = azurerm_private_dns_zone.private_dns_zones["psql"].id
-}
-
-output "private_dns_zone_aoi_id" {
-  description = "ID of the aoi private DNS zone"
-  value       = azurerm_private_dns_zone.private_dns_zones["aoi"].id
-}
-
-output "private_dns_zone_cognitive_services_id" {
-  description = "ID of the cognitive services private DNS zone"
-  value       = azurerm_private_dns_zone.private_dns_zones["cognitive_services"].id
-}
-
-output "private_dns_zone_speech_service_id" {
-  description = "ID of the speech service private DNS zone"
-  value       = azurerm_private_dns_zone.private_dns_zones["speech_service"].id
-}
-
-output "private_dns_zone_storage_id" {
-  description = "ID of the storage private DNS zone"
-  value       = azurerm_private_dns_zone.private_dns_zones["storage"].id
-}
-
-output "private_dns_zone_redis_id" {
-  description = "ID of the redis private DNS zone"
-  value       = azurerm_private_dns_zone.private_dns_zones["redis"].id
-}
-
-output "private_dns_zone_psql_id" {
-  description = "ID of the psql private DNS zone"
-  value       = azurerm_private_dns_zone.private_dns_zones["psql"].id
-}
-
-output "private_dns_zone_links" {
-  description = "ID of the private DNS zone links"
-  value       = azurerm_private_dns_zone_virtual_network_link.private_dns_zone_links
-}
-
-output "private_dns_zone_links_ids" {
-  description = "ID of the private DNS zone links"
-  value       = azurerm_private_dns_zone_virtual_network_link.private_dns_zone_links[*].id
 }
