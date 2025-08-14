3f9a667eb6e14fffab0921ca09476cb80a4f3b9d
Serghei Goineanu <serghei@unique.ch>
2025-08-14 16:10:30 +0200

    Add configuration for client secret management and role assignments in application registration module

--- terraform-modules/identities/application-registration.tf
+++ terraform-modules/identities/application-registration.tf
@@ -10,6 +10,14 @@ resource "azuread_service_principal" "msgraph" {
 module "application_registration" {
   source       = "github.com/Unique-AG/terraform-modules.git//modules/azure-entra-app-registration?ref=azure-entra-app-registration-3.0.0"
   display_name = var.application_registration_gitops_display_name
+  # Keep SP behavior consistent with previous versions
+  role_assignments_required = false
+  # Ensure the client secret stays managed to avoid destroy
+  client_secret_generation_config = {
+    enabled     = true
+    keyvault_id = var.main_kv_id
+    secret_name = var.application_secret_display_name
+  }
   redirect_uris = [
     "https://argo.${var.dns_zone_name}/auth/callback",
   ]
