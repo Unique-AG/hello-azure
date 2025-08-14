e061fa7ac97bbe51f320a0b63350daae5729e6c2
Serghei Goineanu <serghei@unique.ch>
2025-08-14 11:06:02 +0200

    Update Kubernetes module to include Defender Log Analytics workspace ID and maintain existing Prometheus recording rules configuration

--- terraform-modules/workloads/kubernetes.tf
+++ terraform-modules/workloads/kubernetes.tf
@@ -14,10 +14,10 @@ module "kubernetes_cluster" {
   default_subnet_pods_id                  = var.subnet_aks_pods_id
   tags                                    = var.tags
   tenant_id                               = data.azurerm_client_config.current.tenant_id
-
-  prometheus_node_recording_rules       = var.prometheus_node_recording_rules
-  prometheus_kubernetes_recording_rules = var.prometheus_kubernetes_recording_rules
-  prometheus_ux_recording_rules         = var.prometheus_ux_recording_rules
+  defender_log_analytics_workspace_id     = "/subscriptions/be4e82ff-bf83-4173-a750-15e807845579/resourcegroups/DefaultResourceGroup-SEC/providers/Microsoft.OperationalInsights/workspaces/DefaultWorkspace-be4e82ff-bf83-4173-a750-15e807845579-SEC"
+  prometheus_node_recording_rules         = var.prometheus_node_recording_rules
+  prometheus_kubernetes_recording_rules   = var.prometheus_kubernetes_recording_rules
+  prometheus_ux_recording_rules           = var.prometheus_ux_recording_rules
 
   log_analytics_workspace = {
     id                  = var.log_analytics_workspace_id
