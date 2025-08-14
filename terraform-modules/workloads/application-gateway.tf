afe150e85ff2e5c935f29cfbefd4ceab9bb7d378
Serghei Goineanu <serghei@unique.ch>
2025-08-14 14:15:58 +0200

    Refactor application gateway module configuration in Terraform files by restructuring parameters for improved clarity and maintainability.

--- terraform-modules/workloads/application-gateway.tf
+++ terraform-modules/workloads/application-gateway.tf
@@ -1,14 +1,19 @@
 module "application_gateway" {
-  source                     = "github.com/Unique-AG/terraform-modules.git//modules/azure-application-gateway?ref=azure-application-gateway-4.0.0-rc.2"
-  tags                       = var.tags
-  name_prefix                = var.name_prefix
-  resource_group_name        = data.azurerm_resource_group.core.name
-  resource_group_location    = data.azurerm_resource_group.core.location
-  subnet_appgw               = var.subnet_agw_id
-  gateway_mode               = "Detection"
-  gateway_sku                = "WAF_v2"
-  gateway_tier               = "WAF_v2"
-  private_ip                 = cidrhost(var.subnet_agw_cidr, 6)
-  log_analytics_workspace_id = var.log_analytics_workspace_id
-  ip_name                    = var.ip_name
+  source = "github.com/Unique-AG/terraform-modules.git//modules/azure-application-gateway?ref=azure-application-gateway-4.0.0-rc.2"
+
+  resource_group = {
+    name     = data.azurerm_resource_group.core.name
+    location = data.azurerm_resource_group.core.location
+  }
+
+  gateway_ip_configuration = {
+    name      = "gateway-ip-configuration"
+    subnet_id = var.subnet_agw_id
+  }
+
+  public_frontend_ip_configuration = {
+    name = var.ip_name
+  }
+
+  tags = var.tags
 }
