d206f20f37b22c5c22b7f09f92e456903017c9f5
Serghei Goineanu <serghei@unique.ch>
2025-08-14 16:20:05 +0200

    Fix resource group name reference in application gateway module to ensure correct public IP data source usage.

--- terraform-modules/workloads/application-gateway.tf
+++ terraform-modules/workloads/application-gateway.tf
@@ -1,6 +1,6 @@
 data "azurerm_public_ip" "application_gateway_public_ip" {
   name                = var.ip_name
-  resource_group_name = var.resource_group_core_location
+  resource_group_name = var.resource_group_core_name
 }
 
 module "application_gateway" {
