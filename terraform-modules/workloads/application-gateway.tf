07915c29e5c5b086993e5e4e6245f88a6535cb66
Serghei Goineanu <serghei@unique.ch>
2025-08-14 14:19:46 +0200

    Add name_prefix parameter to application gateway module for enhanced configuration flexibility

--- terraform-modules/workloads/application-gateway.tf
+++ terraform-modules/workloads/application-gateway.tf
@@ -11,6 +11,8 @@ module "application_gateway" {
     max_capacity = 2
   }
 
+  name_prefix = var.name_prefix
+
   resource_group = {
     name     = data.azurerm_resource_group.core.name
     location = data.azurerm_resource_group.core.location
