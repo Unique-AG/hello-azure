870e7dc382100301e64d1f6c454bf647dd9864ef
Serghei Goineanu <serghei@unique.ch>
2025-08-15 10:56:30 +0200

    Remove point_in_time_restore_days configuration from storage account modules to simplify settings.

--- terraform-modules/workloads/storage-accounts.tf
+++ terraform-modules/workloads/storage-accounts.tf
@@ -18,7 +18,6 @@ module "ingestion_cache" {
     versioning_enabled                   = false
     container_soft_delete_retention_days = 7
     blob_soft_delete_retention_days      = 7
-    point_in_time_restore_days           = null
   }
 
   storage_management_policy_default = {
@@ -64,7 +63,6 @@ module "ingestion_storage" {
     versioning_enabled                   = false
     container_soft_delete_retention_days = 7
     blob_soft_delete_retention_days      = 7
-    point_in_time_restore_days           = null
   }
 
   storage_management_policy_default = {
