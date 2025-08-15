778b4acd385974f725a480f3f47884b15a4f8299
Serghei Goineanu <serghei@unique.ch>
2025-08-15 12:00:18 +0200

    Update storage account module source references to version 3.0.2 and correct change feed retention parameter for consistency.

--- terraform-modules/workloads/storage-accounts.tf
+++ terraform-modules/workloads/storage-accounts.tf
@@ -1,7 +1,7 @@
 #tfsec:ignore:azure-keyvault-content-type-for-secret
 #tfsec:ignore:azure-keyvault-ensure-key-expiry
 module "ingestion_cache" {
-  source = "github.com/unique-ag/terraform-modules.git//modules/azure-storage-account?depth=1&ref=vsc807-violet-mighty-meadowlark"
+  source = "github.com/unique-ag/terraform-modules.git//modules/azure-storage-account?depth=1&ref=azure-storage-account-3.0.2"
 
   name                          = var.ingestion_cache_sa_name
   resource_group_name           = data.azurerm_resource_group.sensitive.name
@@ -47,7 +47,7 @@ module "ingestion_cache" {
 #tfsec:ignore:azure-keyvault-content-type-for-secret
 #tfsec:ignore:azure-keyvault-ensure-key-expiry
 module "ingestion_storage" {
-  source = "github.com/unique-ag/terraform-modules.git//modules/azure-storage-account?depth=1&ref=vsc807-violet-mighty-meadowlark"
+  source = "github.com/unique-ag/terraform-modules.git//modules/azure-storage-account?depth=1&ref=azure-storage-account-3.0.2"
 
   name                          = var.ingestion_storage_sa_name
   resource_group_name           = data.azurerm_resource_group.sensitive.name
