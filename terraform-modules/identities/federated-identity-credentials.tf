2750314d1c941ac41d96a8b8bb3350a848cfe47b
Hrvoje Kusanic <hrvoje.kusanic@unique.ai>
2025-11-12 16:40:11 +0100

    chore: cleanup

--- terraform-modules/identities/federated-identity-credentials.tf
+++ terraform-modules/identities/federated-identity-credentials.tf
@@ -7,7 +7,7 @@ resource "azurerm_federated_identity_credential" "afic_workloads" {
   issuer              = data.azurerm_kubernetes_cluster.cluster.oidc_issuer_url
   parent_id           = azurerm_user_assigned_identity.aks_workload_identity.id
   subject             = "system:serviceaccount:${each.value.namespace}:${each.value.name}"
-  depends_on = [	
-    var.cluster_id	
+  depends_on = [
+    var.cluster_id
   ]
 }
